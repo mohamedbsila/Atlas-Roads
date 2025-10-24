54914dfee64132d9a37c42fd84f02c99091fa219
habib.ben.jdidia <benjdidiahabib2020@gmail.com>
2025-10-24 05:17:29 +0100

    Fix merge conflict markers in User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -91,7 +91,6 @@ class User extends Authenticatable implements CanResetPasswordContract
     {
         return $this->hasMany(BorrowRequest::class, 'owner_id');
     }
-<<<<<<< HEAD
 
     /**
      * Check if the user is an admin
@@ -102,6 +101,4 @@ class User extends Authenticatable implements CanResetPasswordContract
     {
         return (bool) $this->is_admin;
     }
-=======
->>>>>>> origin/books
 }
